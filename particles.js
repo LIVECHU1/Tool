4ca58d2cadbd6dd01406822b9f83b098a480325f
LIVECHU1 <cokkyk300@gmail.com>
2025-09-14 21:36:16 +0700

    Add files via upload

---
+++ particles.js
@@ -0,0 +1,13 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": { "value": 50 },
+    "color": { "value": "#00ff00" },
+    "shape": { "type": "circle" },
+    "opacity": { "value": 0.3 },
+    "size": { "value": 3 },
+    "line_linked": { "enable": false },
+    "move": { "enable": true, "speed": 1, "direction": "none", "random": true }
+  },
+  "interactivity": { "events": { "onhover": { "enable": false }, "onclick": { "enable": false } } },
+  "retina_detect": true
+});
